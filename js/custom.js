a253329a1dce54785864a542d91bb9b0dcfb6834
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-09-29 03:46:29 +0900

    Updated.

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
-var wrapper = $('<span class="qbox-wrapper"></span>');
-var space = $('<span class="qbox-default"></span>');
-space.html('　　　');
 $(".qbox").each(function(i, o){
+  let wrapper = $('<span class="qbox-wrapper"></span>');
+  let space   = $('<span class="qbox-default"></span>');
+  space.html('　　　');
   let wrapped = $(o).wrap(wrapper);
   wrapped.ready(function() {
     wrapped.after(space);
