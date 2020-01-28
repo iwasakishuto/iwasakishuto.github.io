ec57127aa41a19b2cfa97d4f48ece9076a942496
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-01-28 14:45:14 +0900

    Upated.

--- js/custom.js
+++ js/custom.js
@@ -72,3 +72,21 @@ function switchVisibility(target_id){
     button.style.borderColor = "#c9485b";
   }
 }
+
+function dateToStr24HPad0(date, format) {
+  // Ref: https://www.sejuku.net/blog/22867
+  if (!format) {
+    // デフォルト値
+    format = 'YYYY/MM/DD hh:mm:ss'
+  }
+
+  // フォーマット文字列内のキーワードを日付に置換する
+  format = format.replace(/YYYY/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+
+  return format;
+}
