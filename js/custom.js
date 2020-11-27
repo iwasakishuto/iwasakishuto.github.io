8b2377ecb0e3a2b53e55b237bc3439ce76bc0c2f
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-11-27 15:48:43 +0900

    Add subtext

--- js/custom.js
+++ js/custom.js
@@ -159,10 +159,12 @@ function plot_tree(tree_path,
                    id='tree-wrapper',
                    rect_size={"height": 30, "width": 80}, 
                    node_space={"padding": 30, "height": 50, "width": 120},
-                   text_func=function(d){ return d.data.name; },
+                   main_text_func=function(d){ return d.data.name; },
+                   main_text_color_func=function(d){ return "#000"; },
+                   sub_text_func=function(d){ return d.data.info ; },
+                   sub_text_color_func=function(d){ return "#333"; },
                    node_fillcolor_func=function(d){ return "#fff"; },
                    node_arialabel_func=function(d){ return d.data.note; },
-                   text_fillcolor_func=function(d){ return "#000"; },
                    node_id_assign_func=function(d) { return d.data.name; },
                    ){
   $.getJSON(tree_path, (data) => {
@@ -241,17 +243,23 @@ function plot_tree(tree_path,
         .attr('width', rect_size.width)
         .attr('height', rect_size.height)
         .attr('rx', 3)
-        .attr('fill', node_arialabel_func)
+        .attr('fill', node_fillcolor_func)
+        .attr('aria-label', node_arialabel_func)
+        .attr('id', node_id_assign_func)
+        .attr('class', 'tree-caption')
         .attr('stroke', 'black')
     node
       .append('a')
       .attr('xlink:href', function(d) { return d.data.url; })
-      .attr('id', node_id_assign_func)
-      .attr('class', 'tree-caption')
-      .attr('aria-label', text_fillcolor_func)
       .append('text')
-      .text(text_func)
-      .attr('fill', node_fillcolor_func)
-      .attr('transform', `translate(5, 15)`);
+      .text(main_text_func)
+      .attr('fill', main_text_color_func)
+      .attr('transform', `translate(5, 15)`)
+      .attr('font-size', 15)
+      .append('text')
+      .text(sub_text_func)
+      .attr('fill', sub_text_color_func)
+      .attr('transform', `translate(5, 25)`)
+      .attr('font-size', 10);
   })
 }
